72148057a87c3f32fbc1ffa9d4c78723378beba5
weijien1994 <58802081+weijien1994@users.noreply.github.com>
2026-02-26 10:56:54 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,6 +29,8 @@ resource "aws_instance" "blog" {                 # aws_instance recourse with na
   ami           = data.aws_ami.app_ami.id        # Base image ID, refer to data block ID 
   instance_type = var.instance_type              # instance type refer to variable.tf 
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "HelloWorld"
   }
